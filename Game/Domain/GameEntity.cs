e1ac5117aff12a87acd568863fb7fedbaae92dda
Kkeie <espitsyn74@gmail.com>
2025-10-20 15:29:15 +0500

    1st done

--- Game/Domain/GameEntity.cs
+++ Game/Domain/GameEntity.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Game.Domain
 {
     public class GameEntity
     {
+        [BsonElement]
         private readonly List<Player> players;
 
         public GameEntity(int turnsCount)
             : this(Guid.Empty, GameStatus.WaitingToStart, turnsCount, 0, new List<Player>())
         {
         }
-
+        
+        
+        [BsonConstructor]
         public GameEntity(Guid id, GameStatus status, int turnsCount, int currentTurnIndex, List<Player> players)
         {
             Id = id;
@@ -89,7 +93,14 @@ namespace Game.Domain
                 }
             }
             //TODO Заполнить все внутри GameTurnEntity, в том числе winnerId
-            var result = new GameTurnEntity();
+            var result = new GameTurnEntity
+            {
+                Id = Guid.NewGuid(),
+                GameId = Id,
+                WinnerId = winnerId,
+                TurnInd = CurrentTurnIndex,
+                Decisions = Players.ToDictionary(x => x.UserId, x => x.Decision.GetValueOrDefault())
+            };
             // Это должно быть после создания GameTurnEntity
             foreach (var player in Players)
                 player.Decision = null;
--- Game/Domain/GameTurnEntity.cs
+++ Game/Domain/GameTurnEntity.cs
@@ -1,7 +1,21 @@
+using System;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Options;
+
 namespace Game.Domain
+
+
 {
     public class GameTurnEntity
     {
+        public Guid Id { get; set; }
+        public Guid GameId { get; set; }
+        public int TurnInd { get; set; }
+        public Guid WinnerId { get; set; }
+        [BsonDictionaryOptions(DictionaryRepresentation.ArrayOfArrays)]
+        public Dictionary<Guid, PlayerDecision> Decisions { get; set; }
+        
         //TODO: Придумать какие свойства должны быть в этом классе, чтобы сохранять всю информацию о закончившемся туре.
     }
 }
